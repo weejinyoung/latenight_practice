5fa5ecfdbc3557c96b2da9588193bd9dcd0571a4
Jn W <a8118199@gmail.com>
2022-10-11 20:45:19 +0900

    심야식당 기능공부

---
+++ src/main/java/com/latenight/latenight_practice/LatenightPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.latenight.latenight_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatenightPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatenightPracticeApplication.class, args);
+	}
+
+}
